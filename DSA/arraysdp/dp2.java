59a49d26c18d3ad75bbeb6c6f58b4e86c641d2d2
Ankita <Ankitashejal11@gmail.com>
2024-02-03 14:55:33 +0530

    code updated

--- DSA/arraysdp/dp2.java
+++ DSA/arraysdp/dp2.java
@@ -1,32 +1,47 @@
 package DSA.arraysdp;
 
 public class dp2 {
-    public static void main(String[] args) {
-        int nums[]={1,2,3,4,5,6,7};
-        int target =0;
+    static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
 
-        System.out.println("index of target"+);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Check which half is sorted
+            if (nums[left] <= nums[mid]) {
+                // Left half is sorted
+                if (nums[left] <= target && target < nums[mid]) {
+                    // Target is in the left sorted half
+                    right = mid - 1;
+                } else {
+                    // Target is in the right unsorted half
+                    left = mid + 1;
+                }
+            } else {
+                // Right half is sorted
+                if (nums[mid] < target && target <= nums[right]) {
+                    // Target is in the right sorted half
+                    left = mid + 1;
+                } else {
+                    // Target is in the left unsorted half
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1; // Target not found
     }
 
-    static int search(int nums[],int target){
-     int left =0;
-     int right = nums.length-1;
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
 
-     while(left<right){
-        int mid = left+(right-left)/2;
-        if (nums[mid]==target){
-            return mid;
-        }
-//check which half side is sorted
-       if(nums[left]<=nums[mid]){
-
-        if (nums[left] <= target && target < nums[mid]){//left half is sorted
-right = mid-1;
-        }
-        else{
-            left = mid+1;
-        }
-       }
-     }
+        int result = search(nums, target);
+        System.out.println("Index of target: " + result);
     }
 }

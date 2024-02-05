f9b144b790bdd742744cd4106705825773be8139
Ankita <Ankitashejal11@gmail.com>
2024-02-05 12:44:31 +0530

    selectionsort

--- DSA/sorting/Insertionsort.java
+++ DSA/sorting/Insertionsort.java
@@ -1,29 +1,32 @@
-package DSA.sorting;
 
+package DSA.sorting;
+import java.util.Arrays;
+import java.util.Collections;
 public class Insertionsort {
 
-    static void Isort(int arr[]){
-    for(int i=1;i<arr.length;i++){
-        int curr=arr[i];
-        int prev=i-1;
-        //finding out the correct position to insert
-        while(prev >= 0 && arr[prev] > curr){
-            arr[prev +1] =arr[prev];
-            prev--;
-        }
-        //insertion
-        arr[prev+1]=curr;
-    }
-    }
+    // static void Isort(int arr[]){
+    // for(int i=1;i<arr.length;i++){
+    //     int curr=arr[i];
+    //     int prev=i-1;
+    //     //finding out the correct position to insert
+    //     while(prev >= 0 && arr[prev] > curr){
+    //         arr[prev +1] =arr[prev];
+    //         prev--;
+    //     }
+    //     //insertion
+    //     arr[prev+1]=curr;
+    // }
+    // }
     
-    static void printarr(int arr[]){
-    for(int i=0;i<arr.length;i++){
+    static void printarr(Integer arr[]){
+    for(Integer i=0;i<arr.length;i++){
         System.out.print(arr[i]+" ");
     }
     }
     public static void main(String[] args) {
-        int arr[]={5,4,2,3,1};
-        Isort(arr);
+        Integer arr[]={5,4,2,3,1};
+        // Isort(arr);
+        Arrays.sort(arr,2,5,Collections.reverseOrder());
         printarr(arr);
 
     }
---
+++ Day2/main.java
@@ -0,0 +1,18 @@
+package Day2;
+import java.util.Arrays;
+public class main {
+
+    static void printarr(int[] arr){
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]={13,46,24,52,20,9};
+        Arrays.sort(arr);
+        printarr(arr);
+
+    }
+}
+

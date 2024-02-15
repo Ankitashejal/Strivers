d1b302d0262c30cf2fea8c9dc159eaf45bb85f5d
Ankita <Ankitashejal11@gmail.com>
2024-02-15 18:21:21 +0530

    dp

---
+++ DSA/string/dp4.java
@@ -0,0 +1,10 @@
+package DSA.string;
+
+public class dp4 {
+    public static void main(String[] args) {
+        String str="ApnaCollege".replace("l","");
+        System.out.println(str);
+    }
+       
+
+}

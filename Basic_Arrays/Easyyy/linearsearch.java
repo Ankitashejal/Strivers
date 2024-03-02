850b7202a05607b00463e4d6ec19762a72a651f8
Ankita <Ankitashejal11@gmail.com>
2024-03-03 00:43:56 +0530

    linearsearch.java

---
+++ Basic_Arrays/Easyyy/linearsearch.java
@@ -0,0 +1,9 @@
+public class linearsearch {
+    public static int linearSearch(int n, int num, int []arr){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num)
+                return i;
+        }
+        return -1;
+    }
+}
